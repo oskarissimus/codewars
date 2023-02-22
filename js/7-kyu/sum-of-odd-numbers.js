74521dc6c102627419eed7891a658579f009af60
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-22 12:12:19 +0100

    it works

--- js/7-kyu/sum-of-odd-numbers.js
+++ js/7-kyu/sum-of-odd-numbers.js
@@ -1,3 +1,12 @@
-function rowSumOddNumbers(n) {
-    return Math.pow(n, 3)
+function expandedForm(num) {
+    const output = []
+    const digits = String(num).split('').reverse()
+    for (let i = 0; i < digits.length; i++) {
+        if (digits[i] !== "0") {
+            const outputComponent = String(digits[i]).padEnd(i + 1, "0")
+            output.unshift(outputComponent)
+        }
+
+    }
+    return output.join(" + ")
 }
