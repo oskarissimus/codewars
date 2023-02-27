0da156a810cdb5a2ad58a03966ec051beb40c5a7
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-27 16:15:06 +0100

    basic division

--- rust_solutions/src/kyu_8/mod.rs
+++ rust_solutions/src/kyu_8/mod.rs
@@ -1,7 +1,8 @@
 mod are_you_playing_banjo;
+mod century_from_year;
 mod convert_boolean_values_to_strings_yes_or_no;
 mod grasshopper_basic_function_fixer;
+mod is_he_gonna_survive;
 mod is_this_a_triangle;
 mod opposite_number;
 mod rock_paper_scissors;
-mod century_from_year;
