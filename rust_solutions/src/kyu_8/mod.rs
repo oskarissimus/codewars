726b16e098ebcca870386226ffec83e339b2facd
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-27 16:44:50 +0100

    adding itertools, so tests would run

--- rust_solutions/src/kyu_8/mod.rs
+++ rust_solutions/src/kyu_8/mod.rs
@@ -1,6 +1,7 @@
 mod are_you_playing_banjo;
 mod century_from_year;
 mod convert_boolean_values_to_strings_yes_or_no;
+mod count_by_x;
 mod grasshopper_basic_function_fixer;
 mod is_he_gonna_survive;
 mod is_this_a_triangle;
