e9e16482a5dff5dd20e8d4056c35fe98e55ab11f
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-01 12:44:27 +0100

    regexing

---
+++ rust_solutions/src/kyu_7/regex_validate_pin_code.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+use regex::Regex;
+
+fn validate_pin(pin: &str) -> bool {
+    Regex::new(r"^(\d{4}|\d{6})$").unwrap().is_match(pin)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::validate_pin;
+
+    #[test]
+    fn invalid_length_tests() {
+        assert_eq!(validate_pin("1"), false);
+        assert_eq!(validate_pin("12"), false);
+        assert_eq!(validate_pin("123"), false);
+        assert_eq!(validate_pin("12345"), false);
+        assert_eq!(validate_pin("1234567"), false);
+        assert_eq!(validate_pin("-1234"), false);
+        assert_eq!(validate_pin("1.234"), false);
+        assert_eq!(validate_pin("-1.234"), false);
+        assert_eq!(validate_pin("00000000"), false);
+    }
+
+    #[test]
+    fn non_digit_chars_tests() {
+        assert_eq!(validate_pin("a234"), false);
+        assert_eq!(validate_pin(".234"), false);
+    }
+
+    #[test]
+    fn valid_pin_tests() {
+        assert_eq!(validate_pin("1234"), true);
+        assert_eq!(validate_pin("0000"), true);
+        assert_eq!(validate_pin("1111"), true);
+        assert_eq!(validate_pin("123456"), true);
+        assert_eq!(validate_pin("098765"), true);
+        assert_eq!(validate_pin("000000"), true);
+        assert_eq!(validate_pin("123456"), true);
+        assert_eq!(validate_pin("090909"), true);
+    }
+}
