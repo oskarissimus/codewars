7c4c1073e50a81dfcd7fa8af7a4bcb230ee33eb5
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-05 21:52:30 +0100

    it works for edge cases

--- rust_solutions/src/kyu_6/tortoise_racing.rs
+++ rust_solutions/src/kyu_6/tortoise_racing.rs
@@ -3,11 +3,22 @@ fn race(v1: i32, v2: i32, g: i32) -> Option<Vec<i32>> {
     if v1 >= v2 {
         return None;
     }
-    let t = g as f64 / (v2 as f64 - v1 as f64);
-    let hours = t;
-    let minutes = hours.fract() * 60.0;
-    let seconds = minutes.fract() * 60.0;
-    Some([hours as i32, minutes as i32, seconds as i32].to_vec())
+    let v1_fps = (v1 as f64) / 3600.0;
+    let v2_fps = (v2 as f64) / 3600.0;
+    let t = (g as f64 / (v2_fps - v1_fps)) as i32;
+    let h = extract_hours(t);
+    let m = extract_minutes(t);
+    let s = extract_seconds(t);
+    Some(vec![h, m, s])
+}
+fn extract_hours(t: i32) -> i32 {
+    t / 3600
+}
+fn extract_minutes(t: i32) -> i32 {
+    (t / 60) % 60
+}
+fn extract_seconds(t: i32) -> i32 {
+    t % 60
 }
 
 #[test]
@@ -15,5 +26,6 @@ fn basic_tests() {
     assert_eq!(race(720, 850, 70), Some(vec![0, 32, 18]));
     assert_eq!(race(80, 100, 40), Some(vec![2, 0, 0]));
     assert_eq!(race(80, 91, 37), Some(vec![3, 21, 49]));
+    assert_eq!(race(820, 850, 550), Some(vec![18, 20, 0]));
     assert_eq!(race(820, 81, 550), None);
 }
