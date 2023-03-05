a264a5f33190266f5571a945ddef1388176e93af
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-05 22:03:07 +0100

    tight solution

--- rust_solutions/src/kyu_6/tortoise_racing.rs
+++ rust_solutions/src/kyu_6/tortoise_racing.rs
@@ -4,20 +4,7 @@ fn race(v1: i32, v2: i32, g: i32) -> Option<Vec<i32>> {
         return None;
     }
     let t = g * 3600 / (v2 - v1);
-    let h = extract_hours(t);
-    let m = extract_minutes(t);
-    let s = extract_seconds(t);
-    // println!("{}, {}, {}",v1,v2,g);
-    Some(vec![h, m, s])
-}
-fn extract_hours(t: i32) -> i32 {
-    t / 3600
-}
-fn extract_minutes(t: i32) -> i32 {
-    (t / 60) % 60
-}
-fn extract_seconds(t: i32) -> i32 {
-    t % 60
+    Some(vec![t / 3600, (t / 60) % 60, t % 60])
 }
 
 #[test]
