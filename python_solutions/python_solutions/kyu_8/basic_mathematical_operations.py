7a91b0eb7ce2a1ae8763a10b1c6eb23171cf59d7
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-14 13:27:51 +0100

    including tests in solution

--- python_solutions/python_solutions/kyu_8/basic_mathematical_operations.py
+++ python_solutions/python_solutions/kyu_8/basic_mathematical_operations.py
@@ -6,3 +6,14 @@ def basic_op(operator, value1, value2):
         "-": lambda a,b: a-b,
     }
     return operator_to_function[operator](value1,value2)
+
+import codewars_test as test
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(basic_op('+', 4, 7), 11)
+        test.assert_equals(basic_op('-', 15, 18), -3)
+        test.assert_equals(basic_op('*', 5, 5), 25)
+        test.assert_equals(basic_op('/', 49, 7), 7)
