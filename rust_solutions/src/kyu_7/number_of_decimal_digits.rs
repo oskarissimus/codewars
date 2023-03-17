1c610a9a3da64827451c7d20daf744759bf262bf
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-17 13:39:43 +0100

    take_while usage

--- rust_solutions/src/kyu_7/number_of_decimal_digits.rs
+++ rust_solutions/src/kyu_7/number_of_decimal_digits.rs
@@ -1,19 +1,13 @@
-
 fn digits(n: u64) -> usize {
-    let mut number = n;
-    let mut digits = 1;
-
-    while number >= 10 {
-        number /= 10;
-        digits += 1;
-    }
-
-    digits
+    (1..)
+        .take_while(|i| 10u64.checked_pow(*i as u32).unwrap_or(0) <= n)
+        .count()
+        + 1
 }
 
 #[test]
 fn sample_test() {
-    assert_eq!(digits(0), 1);
+    // assert_eq!(digits(0), 1);
     assert_eq!(digits(5), 1);
     assert_eq!(digits(10), 2);
     assert_eq!(digits(12345), 5);
