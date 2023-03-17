4a2c1a91c403fa85816f28bc6f2826b86481b304
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-17 07:07:24 +0100

    c-style implementation

--- rust_solutions/src/kyu_7/number_of_decimal_digits.rs
+++ rust_solutions/src/kyu_7/number_of_decimal_digits.rs
@@ -1,14 +1,14 @@
+
 fn digits(n: u64) -> usize {
-    if n == 0 {
-        return 1;
-    }
-    let mut d = 0;
     let mut number = n;
-    while number > 0 {
+    let mut digits = 1;
+
+    while number >= 10 {
         number /= 10;
-        d += 1;
+        digits += 1;
     }
-    d
+
+    digits
 }
 
 #[test]
@@ -18,4 +18,5 @@ fn sample_test() {
     assert_eq!(digits(10), 2);
     assert_eq!(digits(12345), 5);
     assert_eq!(digits(9876543210), 10);
+    assert_eq!(digits(999999999999999), 15);
 }
