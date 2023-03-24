79b923e96036f454e00abb9957cef1cbb866a63f
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-25 00:13:27 +0100

    iterationg over alphabet and returning if no matches found

---
+++ js/6-kyu/detect-pangram.js
@@ -0,0 +1,25 @@
+function isPangram(string) {
+    let alphabet = ''
+    for (let charCode = 'a'.charCodeAt(); charCode <= 'z'.charCodeAt(); charCode++) {
+        alphabet += String.fromCharCode(charCode)
+    }
+    for (letter of alphabet) {
+        if (!string.match(letter)) return false
+    }
+    return true
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+    it("test1", () => {
+        var string = "The quick brown fox jumps over the lazy dog."
+        assert.strictEqual(isPangram(string), true)
+    })
+    it("test2", () => {
+        var string = "This is not a pangram."
+        assert.strictEqual(isPangram(string), false)
+    });
+});
