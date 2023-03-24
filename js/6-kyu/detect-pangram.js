26bb88fbcb3917613d4e46d85623713a27ae07cd
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-25 00:15:13 +0100

    added breaking test

--- js/6-kyu/detect-pangram.js
+++ js/6-kyu/detect-pangram.js
@@ -21,5 +21,10 @@ describe("Tests", () => {
     it("test2", () => {
         var string = "This is not a pangram."
         assert.strictEqual(isPangram(string), false)
-    });
+    })
+    it("test3", () => {
+        var string = "Cwm fjord bank glyphs vext quiz"
+        assert.strictEqual(isPangram(string), true)
+    })
+
 });
