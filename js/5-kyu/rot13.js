721a9687539e6f795937f72780d633105df81788
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-24 23:53:55 +0100

    rot 13 implementation

---
+++ js/5-kyu/rot13.js
@@ -0,0 +1,22 @@
+function rot13(message) {
+    return message.split("").map(letter => rotateLetter(letter)).join("")
+}
+
+function rotateLetter(letter) {
+    const isUpperCase = letter.toUpperCase() == letter
+    const first = (isUpperCase ? 'A' : 'a').charCodeAt()
+    const charCode = letter.charCodeAt()
+    const rotatedCharCode = ((charCode - first) + 13) % 26 + first
+    return String.fromCharCode(rotatedCharCode)
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("Tests", function () {
+    it("Sample tests", function () {
+        for (const [input, expected] of [["test", "grfg"], ["Test", "Grfg"]]) {
+            assert.strictEqual(rot13(input), expected, `Test failed with messsage = '${input}'`);
+        }
+    });
+});
