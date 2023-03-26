82b3d769ff2cd22c65b6abe9e7c8bae639d9e448
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-27 00:22:25 +0200

    basic logic

---
+++ typescript_solutions/src/kyu-8/student-s-final-grade.ts
@@ -0,0 +1,16 @@
+export function finalGrade(exam: number, projects: number): number {
+    if (exam > 90 || projects > 10) return 100
+    if (exam > 70 && projects >= 5) return 90
+    if (exam > 50 && projects >= 2) return 75
+    return 0
+}
+
+import { assert } from "chai";
+
+
+describe("Student's Final Grade", () => {
+    it("Fixed tests", () => {
+        assert.strictEqual(finalGrade(100, 12), 100);
+        assert.strictEqual(finalGrade(85, 5), 90);
+    });
+});
