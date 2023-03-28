b2239260784c371499cbd3f149c05d2122a9411d
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-28 14:20:26 +0200

    using scan instead of fold

--- rust_solutions/src/kyu_6/equal_sides_of_an_array.rs
+++ rust_solutions/src/kyu_6/equal_sides_of_an_array.rs
@@ -1,17 +1,21 @@
-use std::ops::Add;
+use itertools::Itertools;
 
 fn find_even_index(arr: &[i32]) -> Option<usize> {
-    let left = arr.iter().fold(vec![0], |partial_sums, n| {
-        let mut new_sum = partial_sums.clone();
-        new_sum.push(partial_sums.last().unwrap().add(n));
-        new_sum
-    });
-    let mut right = vec![];
-    let mut sum = 0;
-    for i in (0..arr.len()).rev() {
-        right.push(sum);
-        sum += arr[i];
-    }
+    let left = arr
+        .iter()
+        .scan(0, |partial_sum, &n| {
+            *partial_sum += n;
+            Some(*partial_sum)
+        })
+        .collect_vec();
+    let mut right = arr
+        .iter()
+        .rev()
+        .scan(0, |partial_sum, &n| {
+            *partial_sum += n;
+            Some(*partial_sum)
+        })
+        .collect_vec();
     right.reverse();
 
     for i in 0..arr.len() {
